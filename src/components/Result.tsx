2076d288dab34e487b3b7702e88164a87e76cbbe
aung_khaing_khant <aungkhaingkhant.dev@gmail.com>
2023-10-18 15:33:03 +0630

    Added Proverb Description.

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,7 +2,14 @@ function Result(props:any){
 
     var k = props.proverbs.map(
         (value:any) =>{
-            return <li>{value.ProverbName}</li>
+            return (
+                <div>
+                <li><b>{value.ProverbName}</b></li>
+                <ul>
+                    <li>{value.ProverbDesp}</li>
+                </ul>
+            </div>
+            )
         }
     )
 
